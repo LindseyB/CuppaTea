3444e6b9853cb2f02bccc3b4dc0fdaa99b80423c
Lindsey Bieda <hazardous.narayan@gmail.com>
2015-06-04 12:06:34 -0400

    Adding basic custom cursor using some default textures

---
+++ Assets/Scripts/HandCursor.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HandCursor : MonoBehaviour {
+	[SerializeField] private Texture2D customCursor;
+
+	// Use this for initialization
+	void Start () {
+		Cursor.visible = false;
+	}
+
+	void OnGUI () {
+		GUI.DrawTexture(new Rect(
+			Input.mousePosition.x - customCursor.width/2,
+			Screen.height - Input.mousePosition.y - customCursor.height/2,
+			customCursor.width,
+			customCursor.height), 
+		    customCursor, 
+		    ScaleMode.ScaleToFit, 
+		    true);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
