febd12ebc572f85c3e8b4e90ccbd5fc1c2ae420f
Lindsey Bieda <hazardous.narayan@gmail.com>
2015-06-08 18:49:14 -0400

    Adding steam and making the water boil and cool
    Closes #16 and closes #12

--- Assets/Scripts/Objects/Kettle.cs
+++ Assets/Scripts/Objects/Kettle.cs
@@ -2,7 +2,47 @@
 using System.Collections;
 
 public class Kettle : MonoBehaviour, Usable {
+	[SerializeField] private ParticleSystem steam;
+
+	private bool heating;
+	private int temp;
+	private float timer;
+
+	void Start () {
+		heating = false;
+		timer = 1;
+		temp = 20;
+	}
+
 	public void Use() {
-		Debug.Log("Use this object");
+		// toggle heating
+		heating = !heating;
+	}
+
+	void Update() {
+		// if we are heating or cooling
+		if (temp != 20 || heating) {
+			timer -= Time.deltaTime;
+
+			if (timer <= 0) {
+				updateTemp();
+				timer = 1;
+			}
+		}
+
+		// only play the steam when temp is above boiling
+		if (temp >= 100 && steam.isStopped) {
+			steam.Play();
+		} else if (temp < 100 && steam.isPlaying) {
+			steam.Stop();
+		}
+	}
+
+	private void updateTemp(){
+		if (heating) {
+			temp++;
+		} else if (temp > 20) {
+			temp--;
+		}
 	}
 }
