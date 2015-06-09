05d05497ef38f4e855e6100301e55505c1b0b104
Lindsey Bieda <hazardous.narayan@gmail.com>
2015-06-09 15:47:19 -0400

    Adding tin and fixing collisions with table
    Closes #7

--- Assets/Scripts/Player/MoveCamera.cs
+++ Assets/Scripts/Player/MoveCamera.cs
@@ -2,25 +2,42 @@
 using System.Collections;
 
 public class MoveCamera : MonoBehaviour {
+	[SerializeField] private GameObject boundsObject;
+
 	void Update () {
+		Vector3 position = gameObject.transform.position;
+
 		if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W)) {
 			gameObject.transform.Translate(Vector3.forward * Time.deltaTime);
+			boundsCheck(position);
 		}
 
 		if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S)) {
 			gameObject.transform.Translate(Vector3.back * Time.deltaTime);
+			boundsCheck(position);
 		}
 
 		if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D)) {
 			gameObject.transform.Translate(Vector3.right * Time.deltaTime);
+			boundsCheck(position);
 		}
 
 		if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A)) {
 			gameObject.transform.Translate(Vector3.left * Time.deltaTime);
+			boundsCheck(position);
 		}
 
 		if (Input.GetKey(KeyCode.Space)) {
 			gameObject.transform.Translate(Vector3.up * Time.deltaTime);
+			boundsCheck(position);
 		}
 	}
+
+	void boundsCheck(Vector3 position) {
+		if(gameObject.GetComponent<CharacterController>().bounds.Intersects(boundsObject.GetComponent<Renderer>().bounds)){
+			// if intersecting bounds object move back
+			gameObject.transform.position = position;			
+		}	
+	}
 }
+
