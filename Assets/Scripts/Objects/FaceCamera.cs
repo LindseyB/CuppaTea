7d8fa4bf1ddba2a705496431faa07d26a20e04cb
Lindsey Bieda <hazardous.narayan@gmail.com>
2015-06-13 01:19:07 -0400

    Disable the label if the tin is on the side.
    Todd is telling me to go to bed. Otherwise there'd be mor ein this.

--- Assets/Scripts/Objects/FaceCamera.cs
+++ Assets/Scripts/Objects/FaceCamera.cs
@@ -2,12 +2,22 @@
 using System.Collections;
 
 public class FaceCamera : MonoBehaviour {
-	
+	private GameObject parent;
 
-	// Update is called once per frame
+	void Start() {
+		parent = gameObject.transform.parent.gameObject;
+	}
+
 	void Update () {
 		Vector3 forward = Camera.main.transform.forward;
 		forward.y = 0.0f;
+
+		if(parent.transform.forward.y < 1){
+			// turn off if on side
+			gameObject.SetActive(false);
+		}
+
+
 		gameObject.transform.rotation = Quaternion.LookRotation(forward);
 	}
 }
