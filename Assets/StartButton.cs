93a08789688c82d85e2ecc7dfafd149008ccf663
Lindsey Bieda <hazardous.narayan@gmail.com>
2015-06-21 11:27:39 -0400

    Adding some fonts and fancy fading to start menu

--- Assets/StartButton.cs
+++ Assets/StartButton.cs
@@ -4,16 +4,41 @@ using System.Collections;
 
 
 public class StartButton : MonoBehaviour {
+	[SerializeField] private Color startColor;
+	[SerializeField] private Color hoverColor;
+
 	private GameState gameState;
+	private Text buttonText;
 
 	void Start(){
+		buttonText = gameObject.transform.GetChild (0).GetComponent<Text> ();
 		gameState = FindObjectOfType (typeof(GameState)) as GameState;
+
+		buttonText.color = startColor;
 		gameObject.GetComponent<Button>().onClick.AddListener(() => { startGame(); }); 
 	}
 
 	private void startGame(){
 		gameState.InMainMenu = false;
-		GameObject.Find ("FirstPersonCharacter").GetComponent<UnityStandardAssets.ImageEffects.VignetteAndChromaticAberration>().enabled = false;
+		foreach (Text text in GameObject.Find("Canvas").GetComponentsInChildren<Text>()) {
+			text.CrossFadeAlpha (0.0f, 0.5f, true);
+		}
+		StartCoroutine("DisableCanvas");
+	}
+	
+
+	IEnumerator DisableCanvas() {
+		yield return new WaitForSeconds(1);
 		GameObject.Find("Canvas").SetActive(false);
+		GameObject.Find("FirstPersonCharacter").GetComponent<UnityStandardAssets.ImageEffects.VignetteAndChromaticAberration>().enabled = false;
+		yield return null;
+	}
+
+	public void onEnter() {
+		buttonText.CrossFadeColor(hoverColor, 0.5f, true, true);
+	}
+
+	public void onLeave() {
+		buttonText.CrossFadeColor(startColor, 0.5f, true, true);
 	}
 }
