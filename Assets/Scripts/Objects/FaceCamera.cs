899d653cf4a4cd338550919407d10e4b5255fcd8
Lindsey Bieda <hazardous.narayan@gmail.com>
2015-06-29 23:19:56 -0400

    Update fonts and enlarge fonts with distance

--- Assets/Scripts/Objects/FaceCamera.cs
+++ Assets/Scripts/Objects/FaceCamera.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public class FaceCamera : MonoBehaviour {
 	private GameObject parent;
+	private float dist;
 
 	void Start() {
 		parent = gameObject.transform.parent.gameObject;
@@ -11,6 +12,9 @@ public class FaceCamera : MonoBehaviour {
 	void Update () {
 		Vector3 forward = Camera.main.transform.forward;
 		forward.y = 0.0f;
+
+		dist = Vector3.Distance(Camera.main.transform.position, gameObject.transform.position);
+		gameObject.GetComponent<TextMesh>().fontSize = 60 + (int)(dist*20);
 
 		if(parent.transform.forward.y < 1){
 			// turn off if on side
