066bb91736387405d4f98d99e2e436b005c6458c
Lindsey Bieda <hazardous.narayan@gmail.com>
2015-07-02 22:43:34 -0400

    Trying to display shader on hover.

---
+++ Assets/Scripts/Objects/HoverHighlight.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HoverHighlight : MonoBehaviour {
+	[SerializeField] public Material hoverMat;
+
+	GameObject hoverObject;
+
+	public void Start() {
+		hoverObject = Instantiate(gameObject) as GameObject;
+		hoverObject.SetActive(false);
+		removeJunk();	
+		hoverObject.GetComponent<Renderer>().material = hoverMat;
+	}
+
+	public void OnMouseEnter() {
+		hoverObject.SetActive(true);
+	}
+
+	public void OnMouseExit() {
+		hoverObject.SetActive(false);
+	}
+
+	void removeJunk() {
+		// remove all scripts
+		foreach(MonoBehaviour script in hoverObject.GetComponents<MonoBehaviour>()) {
+			script.enabled = false;
+		}
+
+		hoverObject.tag = "Untagged";
+		hoverObject.GetComponent<Rigidbody>().isKinematic = true;
+
+		foreach(BoxCollider collider in hoverObject.GetComponents<BoxCollider>()) {
+			collider.enabled = false;
+		}
+	}
+}
---
+++ Assets/Shaders/Silhouette.shader
@@ -0,0 +1,50 @@
+Shader "Custom/Silhouette" {
+Properties  {
+	_MainTex ("RGBA Texture Image", 2D) = "white" {}
+	_Cutoff ("Alpha Cutoff", Float) = 0.5
+}
+
+SubShader { 
+	Pass {
+		ZTest Greater
+		Cull Off
+	 
+		CGPROGRAM
+		#pragma fragment frag
+		#pragma vertex vert alpha
+		#include "UnityCG.cginc"
+
+		uniform sampler2D _MainTex; 
+		uniform float _Cutoff;
+
+		struct v2f {
+			float4 pos : SV_POSITION;
+			float4 tex : TEXCOORD0;
+		};
+
+		struct appdata_t {
+			float4 vertex : POSITION;
+			float4 texcoord : TEXCOORD0;
+		};
+
+		v2f vert (appdata_t v) {
+			v2f o;
+			o.pos = mul (UNITY_MATRIX_MVP, v.vertex);
+			o.tex = v.texcoord;
+			return o;
+		}
+	  
+		half4 frag (v2f i) : COLOR {
+			float4 textureColor = tex2D(_MainTex, i.tex.xy);
+			if (textureColor.a < _Cutoff) {
+				discard;
+			}
+			return textureColor;
+		}
+		
+		ENDCG 
+	}
+}
+
+FallBack "Diffuse"
+}
