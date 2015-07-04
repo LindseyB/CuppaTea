f1ac31b2ed90ad00936423bfa965a815c774f1a1
Lindsey Bieda <hazardous.narayan@gmail.com>
2015-07-04 16:02:33 -0400

    Fixing a bug where a grabbed object could get rehighlighted

--- Assets/Scripts/Objects/HoverHighlight.cs
+++ Assets/Scripts/Objects/HoverHighlight.cs
@@ -5,13 +5,18 @@ public class HoverHighlight : MonoBehaviour {
 	Texture2D overlayTexture;
 	Material material;
 
+	private GrabAndDrop grabber;
+
 	public void Start() {
 		overlayTexture = Resources.Load("stripes") as Texture2D;
 		material = gameObject.GetComponent<Renderer>().material;
+		grabber = FindObjectOfType (typeof(GrabAndDrop)) as GrabAndDrop;
 	}
 
 	public void OnMouseEnter() {
-		material.SetTexture("_Detail", overlayTexture);
+		if (gameObject != grabber.grabbedObject) {
+			material.SetTexture("_Detail", overlayTexture);
+		}
 	}
 
 	public void OnMouseExit() {
