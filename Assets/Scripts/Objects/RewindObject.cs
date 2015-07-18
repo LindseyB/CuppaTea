bb6a1070933778beba6db1062055f165973d3744
Lindsey Bieda <hazardous.narayan@gmail.com>
2015-07-17 23:32:07 -0400

    Lerp to move and remove forces to prevent popping

--- Assets/Scripts/Objects/RewindObject.cs
+++ Assets/Scripts/Objects/RewindObject.cs
@@ -8,6 +8,8 @@ public class RewindObject : MonoBehaviour {
 	private Queue<Vector3> history = new Queue<Vector3>();
 	private Queue<Quaternion> rot_history = new Queue<Quaternion>();
 
+	private bool moving = false;
+
 	public void Update() {
 		if(!rewinding) {
 			if(history.Count == LIMIT) {
@@ -29,8 +31,25 @@ public class RewindObject : MonoBehaviour {
 
 	void Rewind() {
 		if(history.Count > 0){
-			transform.position = history.Dequeue();
-			transform.rotation = rot_history.Dequeue();
+			StartCoroutine(MoveFromTo(history.Dequeue(), rot_history.Dequeue(), 0.5f));
+		}
+	}
+
+	IEnumerator MoveFromTo(Vector3 targetPos, Quaternion targetRot, float time){
+		Vector3 startPos = transform.position;
+		Quaternion startRot = transform.rotation;
+		if (!moving){
+			moving = true;
+			float t = 0f;
+			while (t < 1f){
+				t += Time.deltaTime / time;
+				transform.position = Vector3.Lerp(startPos, targetPos, t); 
+				transform.rotation = Quaternion.Lerp(startRot, targetRot, t);
+				yield return 0;
+			}
+			moving = false;
+			gameObject.GetComponent<Rigidbody>().velocity = Vector3.zero;
+			gameObject.GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
 		}
 	}
 }
