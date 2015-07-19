a7d23c41fce6ba85202d7a2b818847192a1b513e
Lindsey Bieda <hazardous.narayan@gmail.com>
2015-07-19 16:38:00 -0400

    Adding clock animation
    Closes #48

---
+++ Assets/Scripts/Objects/SecondHand.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SecondHand : MonoBehaviour {
+	private bool moving = false;
+	private float degrees = 0;
+
+	void Update () {
+		Quaternion rot = Quaternion.Euler(degrees -= 1f, 0, 0);
+		StartCoroutine(Tick(rot,1f));
+	}
+
+	IEnumerator Tick(Quaternion targetRot, float time){
+		Quaternion startRot = transform.rotation;
+		if (!moving){
+			moving = true;
+			float t = 0f;
+			while (t < 1f){
+				t += Time.deltaTime / time;
+				transform.rotation = Quaternion.Lerp(startRot, targetRot, t);
+				yield return 0;
+			}
+			moving = false;
+		}
+	}
+}
