7ab6f8d9f6ec4a29c32936027b58aa6a159cdc39
Lindsey Bieda <hazardous.narayan@gmail.com>
2015-07-31 21:49:15 -0400

    iMkaing glitch bear more glitchy

---
+++ Assets/Scripts/Objects/GlitchBear.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GlitchBear : MonoBehaviour {
+	float timer = 0.5f;
+
+	void Update () {
+		timer -= Time.deltaTime;
+		if(timer <= 0){
+			gameObject.GetComponent<Renderer>().material.SetTextureOffset("_MainTex", new Vector2(Random.Range(0, 50f), Random.Range(0, 50f)));
+			timer = Random.Range(0.01f, 0.5f);
+		}
+	}
+}
