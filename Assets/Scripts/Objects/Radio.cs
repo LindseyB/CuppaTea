4256cbbf2a87de92c1f15aa4962e30fed8dd34c8
Lindsey Bieda <hazardous.narayan@gmail.com>
2015-07-31 21:09:55 -0400

    Allow audio to be muted and unmuted
    Closes #62

---
+++ Assets/Scripts/Objects/Radio.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Radio : MonoBehaviour, Usable {
+	public void Use() {
+		if(AudioListener.volume > 0f) {
+			AudioListener.volume = 0f;
+		} else {
+			AudioListener.volume = 1f;
+		}
+	}
+}
