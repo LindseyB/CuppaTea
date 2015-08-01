9d2ee4d1db979620071e66a400e75e0df7582ac7
Lindsey Bieda <hazardous.narayan@gmail.com>
2015-08-01 13:31:27 -0400

    Glitch faster

--- Assets/Scripts/Objects/GlitchBear.cs
+++ Assets/Scripts/Objects/GlitchBear.cs
@@ -2,13 +2,13 @@
 using System.Collections;
 
 public class GlitchBear : MonoBehaviour {
-	float timer = 0.5f;
+	float timer = 0.01f;
 
 	void Update () {
 		timer -= Time.deltaTime;
 		if(timer <= 0){
 			gameObject.GetComponent<Renderer>().material.SetTextureOffset("_MainTex", new Vector2(Random.Range(0, 50f), Random.Range(0, 50f)));
-			timer = Random.Range(0.01f, 0.5f);
+			timer = Random.Range(0.01f, 0.1f);
 		}
 	}
 }
