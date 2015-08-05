800a66da97df7cd72d695068012ace5a13e3cda3
Lindsey Bieda <hazardous.narayan@gmail.com>
2015-08-04 23:15:02 -0400

    Make sure the glitch overlay repeats correctly

--- Assets/Scripts/Objects/GlitchBear.cs
+++ Assets/Scripts/Objects/GlitchBear.cs
@@ -5,8 +5,12 @@ public class GlitchBear : MonoBehaviour {
 	public MovieTexture glitch;
 
 	void Start() {
+		// For some reason I need to clamp before repeating - unity bug?
+		glitch.wrapMode = TextureWrapMode.Clamp;
 		gameObject.GetComponent<Renderer>().material.SetTexture("_Detail", glitch);
 		glitch.loop = true;
 		glitch.Play();
+		glitch.wrapMode = TextureWrapMode.Repeat;
+
 	}	
 }
