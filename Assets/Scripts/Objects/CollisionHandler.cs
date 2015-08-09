078324d4e07a48936132c86bb654c1291d28f5d1
Lindsey Bieda <hazardous.narayan@gmail.com>
2015-08-09 11:05:16 -0400

    Adding collision sound to bear

---
+++ Assets/Scripts/Objects/CollisionHandler.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CollisionHandler : MonoBehaviour {
+	[SerializeField] private AudioSource collideAudio;
+
+	void OnCollisionEnter(Collision collision) {
+		if(!GameState.Rewinding && !GameState.InMainMenu){ collideAudio.Play(); }
+	}
+}
