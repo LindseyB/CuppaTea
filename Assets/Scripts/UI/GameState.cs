2748328e5b261a82143c032883e3b92330a19fbe
Lindsey Bieda <hazardous.narayan@gmail.com>
2015-08-25 12:56:09 -0400

    Adds the blackhole code.
    Closes #72

--- Assets/Scripts/UI/GameState.cs
+++ Assets/Scripts/UI/GameState.cs
@@ -4,4 +4,5 @@ using System.Collections;
 public class GameState : MonoBehaviour {
 	public static bool InMainMenu = true;	
 	public static bool Rewinding = false;
+	public static bool Blackhole = false;
 }
