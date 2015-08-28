72b5e07dfdc19279619b622ed5cc75192255cc5b
Lindsey Bieda <hazardous.narayan@gmail.com>
2015-08-27 21:51:16 -0400

    Hack to fix exit crash bug
    Fixes #78

--- Assets/Scripts/Objects/Door.cs
+++ Assets/Scripts/Objects/Door.cs
@@ -2,7 +2,17 @@
 using System.Collections;
 
 public class Door : MonoBehaviour, Usable {
+	void Update() {
+		if (Input.GetKey(KeyCode.Escape)) {
+			Exit();
+		}
+	}
+
 	public void Use() {
-		Application.Quit();
+		Exit();
+	}
+
+	private void Exit() {
+		if (!Application.isEditor) System.Diagnostics.Process.GetCurrentProcess().Kill();
 	}
 }
