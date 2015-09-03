d4c2c1b25e9ee88449df78bf22d1632a96740eee
Lindsey Bieda <hazardous.narayan@gmail.com>
2015-09-03 18:38:22 -0400

    Adding video to credits

---
+++ Assets/Scripts/Bloopers.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Bloopers : MonoBehaviour {
+	void Start () {
+		MovieTexture mt = gameObject.GetComponent<RawImage>().mainTexture as MovieTexture;
+		mt.loop = true;
+		mt.Play();
+	}
+}
